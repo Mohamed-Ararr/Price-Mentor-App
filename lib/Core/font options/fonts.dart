4646387b38557a5a5309ef3f3a5bfa99f34c9a7d
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-03-18 14:09:32 +0000

    add font options file

---
+++ lib/Core/font options/fonts.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Fonts {
+  static TextStyle appTitle = const TextStyle(
+    fontSize: 28,
+    color: Colors.white,
+    letterSpacing: 3,
+  );
+}
--- lib/Features/SplashScreen/Presentation/Widgets/SplashViewBody.dart
+++ lib/Features/SplashScreen/Presentation/Widgets/SplashViewBody.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pricementor/Core/font%20options/fonts.dart';
 import 'package:pricementor/constValues.dart';
 
 class SplashViewBody extends StatelessWidget {
@@ -14,20 +15,26 @@ class SplashViewBody extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
+            const Spacer(),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Image.asset(laptopImage),
+                const SizedBox(width: 10),
                 Container(
                   height: 40,
                   width: 2,
                   color: Colors.red,
                 ),
+                const SizedBox(width: 10),
                 Column(
-                  children: const [
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
                     Text(
                       "Price Mentor",
+                      style: Fonts.appTitle,
                     ),
-                    Text(
+                    const Text(
                       "Smart Predictions, Smart Savings!",
                     )
                   ],
@@ -36,6 +43,7 @@ class SplashViewBody extends StatelessWidget {
             ),
             const Spacer(),
             const Text("Never overpay again with our laptop price insights!"),
+            const SizedBox(height: 15),
           ],
         ),
       ),
