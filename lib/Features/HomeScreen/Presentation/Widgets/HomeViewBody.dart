863cc7d24242e430451ecac34b2f46c052787323
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-03-18 14:34:49 +0000

    add the correct font awesome package

--- lib/Features/HomeScreen/Presentation/Widgets/HomeViewBody.dart
+++ lib/Features/HomeScreen/Presentation/Widgets/HomeViewBody.dart
@@ -1,12 +1,35 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:pricementor/constValues.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: kPaddingLR,
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Image.asset(laptopImage),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.abc),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
