fb62ee0de7d1927537d6a4acc7c6290273bccbde
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-03-18 16:38:00 +0000

    change custom app bar

--- lib/Features/HomeScreen/Presentation/Widgets/HomeViewBody.dart
+++ lib/Features/HomeScreen/Presentation/Widgets/HomeViewBody.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:pricementor/Core/Drop%20down%20search/DropdownSearch.dart';
 import 'package:pricementor/Core/font%20options/fonts.dart';
 import 'package:pricementor/Features/HomeScreen/Presentation/Widgets/HomeViewAppBar.dart';
@@ -19,176 +20,190 @@ class _HomeViewBodyState extends State<HomeViewBody> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: kPaddingLR,
-          child: SingleChildScrollView(
-            physics: const BouncingScrollPhysics(),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const HomeViewAppBar(),
-                const SizedBox(height: 10),
-                Center(
-                  child: Text(
-                    "Enter your desired specs",
-                    style: Fonts.regularTitle,
-                  ),
+      appBar: AppBar(
+        backgroundColor: kBlueColor,
+        title: const Text("Price Mentor"),
+        centerTitle: true,
+        leading: Image.asset(
+          laptopImage,
+          scale: 0.9,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(FontAwesomeIcons.circleInfo),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: kPaddingLR,
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 10),
+              Center(
+                child: Text(
+                  "Enter your desired specs",
+                  style: Fonts.regularTitle,
                 ),
-                const SizedBox(height: 25),
-                Form(
-                  key: key,
-                  child: Column(
-                    children: [
-                      Dropdown(
-                        onSaved: (value) {
-                          brand = value;
-                        },
-                        fieldTitle: "Brand",
-                        hintText: "Choose Brand",
-                        items: ["items"],
-                      ),
-                      const SizedBox(height: 20),
-                      Dropdown(
-                        onSaved: (value) {
-                          brand = value;
-                        },
-                        fieldTitle: "CPU",
-                        hintText: "Choose CPU",
-                        items: ["items"],
-                      ),
-                      const SizedBox(height: 20),
-                      Dropdown(
-                        onSaved: (value) {
-                          brand = value;
-                        },
-                        fieldTitle: "RAM",
-                        hintText: "Choose RAM",
-                        items: ["items"],
-                      ),
-                      const SizedBox(height: 20),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Dropdown(
-                              onSaved: (value) {
-                                brand = value;
-                              },
-                              fieldTitle: "SSD",
-                              hintText: "Choose SSD",
-                              items: ["items"],
-                            ),
+              ),
+              const SizedBox(height: 25),
+              Form(
+                key: key,
+                child: Column(
+                  children: [
+                    Dropdown(
+                      onSaved: (value) {
+                        brand = value;
+                      },
+                      fieldTitle: "Brand",
+                      hintText: "Choose Brand",
+                      items: ["items"],
+                    ),
+                    const SizedBox(height: 20),
+                    Dropdown(
+                      onSaved: (value) {
+                        brand = value;
+                      },
+                      showSearchBox: true,
+                      fieldTitle: "CPU",
+                      hintText: "Choose CPU",
+                      items: ["items"],
+                    ),
+                    const SizedBox(height: 20),
+                    Dropdown(
+                      onSaved: (value) {
+                        brand = value;
+                      },
+                      fieldTitle: "RAM",
+                      hintText: "Choose RAM",
+                      items: ["items"],
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Dropdown(
+                            onSaved: (value) {
+                              brand = value;
+                            },
+                            fieldTitle: "SSD",
+                            hintText: "Choose SSD",
+                            items: ["items"],
                           ),
-                          const SizedBox(width: 20),
-                          Expanded(
-                            child: Dropdown(
-                              onSaved: (value) {
-                                brand = value;
-                              },
-                              fieldTitle: "HDD",
-                              hintText: "Choose HDD",
-                              items: ["items"],
-                            ),
+                        ),
+                        const SizedBox(width: 20),
+                        Expanded(
+                          child: Dropdown(
+                            onSaved: (value) {
+                              brand = value;
+                            },
+                            fieldTitle: "HDD",
+                            hintText: "Choose HDD",
+                            items: ["items"],
                           ),
-                        ],
-                      ),
-                      const SizedBox(height: 20),
-                      Dropdown(
-                        onSaved: (value) {
-                          brand = value;
-                        },
-                        fieldTitle: "GPU",
-                        hintText: "Choose GPU",
-                        items: ["items"],
-                      ),
-                      const SizedBox(height: 20),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Dropdown(
-                              onSaved: (value) {
-                                brand = value;
-                              },
-                              fieldTitle: "Screen Size",
-                              hintText: "Choose Size",
-                              items: ["items"],
-                            ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Dropdown(
+                      onSaved: (value) {
+                        brand = value;
+                      },
+                      showSearchBox: true,
+                      fieldTitle: "GPU",
+                      hintText: "Choose GPU",
+                      items: ["items"],
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Dropdown(
+                            onSaved: (value) {
+                              brand = value;
+                            },
+                            fieldTitle: "Screen Size",
+                            hintText: "Choose Size",
+                            items: ["items"],
                           ),
-                          const SizedBox(width: 20),
-                          Expanded(
-                            child: Dropdown(
-                              onSaved: (value) {
-                                brand = value;
-                              },
-                              fieldTitle: "Resolution",
-                              hintText: "Choose Resolution",
-                              items: ["items"],
-                            ),
+                        ),
+                        const SizedBox(width: 20),
+                        Expanded(
+                          child: Dropdown(
+                            onSaved: (value) {
+                              brand = value;
+                            },
+                            fieldTitle: "Resolution",
+                            hintText: "Choose Resolution",
+                            items: ["items"],
                           ),
-                        ],
-                      ),
-                      const SizedBox(height: 20),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Dropdown(
-                              onSaved: (value) {
-                                brand = value;
-                              },
-                              fieldTitle: "Display Tech",
-                              hintText: "Choose Display",
-                              items: ["items"],
-                            ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Dropdown(
+                            onSaved: (value) {
+                              brand = value;
+                            },
+                            fieldTitle: "Display Tech",
+                            hintText: "Choose Display",
+                            items: ["items"],
                           ),
-                          const SizedBox(width: 20),
-                          Expanded(
-                            child: Dropdown(
-                              onSaved: (value) {
-                                brand = value;
-                              },
-                              fieldTitle: "Refresh Rate",
-                              hintText: "Choose Refresh Rate",
-                              items: ["items"],
-                            ),
+                        ),
+                        const SizedBox(width: 20),
+                        Expanded(
+                          child: Dropdown(
+                            onSaved: (value) {
+                              brand = value;
+                            },
+                            fieldTitle: "Refresh Rate",
+                            hintText: "Choose Refresh Rate",
+                            items: ["items"],
                           ),
-                        ],
-                      ),
-                      const SizedBox(height: 20),
-                      Dropdown(
-                        onSaved: (value) {
-                          brand = value;
-                        },
-                        fieldTitle: "Operating System",
-                        hintText: "Choose OS",
-                        items: ["items"],
-                      ),
-                      const SizedBox(height: 20),
-                      Dropdown(
-                        onSaved: (value) {
-                          brand = value;
-                        },
-                        fieldTitle: "Condition",
-                        hintText: "Choose Condition",
-                        items: ["items"],
-                      ),
-                    ],
-                  ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Dropdown(
+                      onSaved: (value) {
+                        brand = value;
+                      },
+                      fieldTitle: "Operating System",
+                      hintText: "Choose OS",
+                      items: ["items"],
+                    ),
+                    const SizedBox(height: 20),
+                    Dropdown(
+                      onSaved: (value) {
+                        brand = value;
+                      },
+                      fieldTitle: "Condition",
+                      hintText: "Choose Condition",
+                      items: ["New", "Like New", "Good"],
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    if (key.currentState!.validate()) {
-                      key.currentState!.save();
-                      debugPrint(brand);
-                    } else {
-                      debugPrint("faild");
-                    }
-                  },
-                  child: const Text("submit"),
-                ),
-                const SizedBox(height: 10),
-              ],
-            ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (key.currentState!.validate()) {
+                    key.currentState!.save();
+                    debugPrint(brand);
+                  } else {
+                    debugPrint("faild");
+                  }
+                },
+                child: const Text("submit"),
+              ),
+              const SizedBox(height: 10),
+            ],
           ),
         ),
       ),
