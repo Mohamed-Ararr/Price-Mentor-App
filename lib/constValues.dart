ccb4b03be45f6c35db6792d3bbd28cb54b014446
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-03-18 14:32:41 +0000

    add font_awesome package

--- lib/constValues.dart
+++ lib/constValues.dart
@@ -5,3 +5,4 @@ import 'package:flutter/material.dart';
 Color kBlueColor = const Color(0xff0F4C81);
 const String laptopImage = "assets/images/laptop-logo.png";
 Color kOrangeColor = const Color(0xffFFA500);
+const EdgeInsets kPaddingLR = EdgeInsets.symmetric(horizontal: 20);
