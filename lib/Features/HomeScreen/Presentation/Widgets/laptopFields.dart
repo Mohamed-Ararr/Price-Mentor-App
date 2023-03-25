a9ea3c83548ee2956810f3ca9bea49495a49555e
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-03-25 14:02:30 +0000

    provide dropdown with items

--- lib/Features/HomeScreen/Presentation/Widgets/laptopFields.dart
+++ lib/Features/HomeScreen/Presentation/Widgets/laptopFields.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pricementor/Core/Utils/item_list.dart';
 
 import '../../../../Core/Drop down search/DropdownSearch.dart';
 
@@ -13,7 +14,7 @@ class LaptopFields extends StatelessWidget {
           onSaved: (value) {},
           fieldTitle: "Brand",
           hintText: "Choose Brand",
-          items: ["items"],
+          items: ItemsLists.brand,
         ),
         const SizedBox(height: 20),
         Dropdown(
@@ -21,14 +22,14 @@ class LaptopFields extends StatelessWidget {
           showSearchBox: true,
           fieldTitle: "CPU",
           hintText: "Choose CPU",
-          items: ["items"],
+          items: ItemsLists.cpu,
         ),
         const SizedBox(height: 20),
         Dropdown(
           onSaved: (value) {},
           fieldTitle: "RAM",
           hintText: "Choose RAM",
-          items: ["items"],
+          items: ItemsLists.ram,
         ),
         const SizedBox(height: 20),
         Row(
@@ -38,7 +39,7 @@ class LaptopFields extends StatelessWidget {
                 onSaved: (value) {},
                 fieldTitle: "SSD",
                 hintText: "Choose SSD",
-                items: ["items"],
+                items: ItemsLists.ssd,
               ),
             ),
             const SizedBox(width: 20),
@@ -47,7 +48,7 @@ class LaptopFields extends StatelessWidget {
                 onSaved: (value) {},
                 fieldTitle: "HDD",
                 hintText: "Choose HDD",
-                items: ["items"],
+                items: ItemsLists.hdd,
               ),
             ),
           ],
@@ -58,7 +59,7 @@ class LaptopFields extends StatelessWidget {
           showSearchBox: true,
           fieldTitle: "GPU",
           hintText: "Choose GPU",
-          items: ["items"],
+          items: ItemsLists.gpu,
         ),
         const SizedBox(height: 20),
         Row(
@@ -68,7 +69,7 @@ class LaptopFields extends StatelessWidget {
                 onSaved: (value) {},
                 fieldTitle: "Screen Size",
                 hintText: "Choose Size",
-                items: ["items"],
+                items: ItemsLists.size,
               ),
             ),
             const SizedBox(width: 20),
@@ -77,46 +78,33 @@ class LaptopFields extends StatelessWidget {
                 onSaved: (value) {},
                 fieldTitle: "Resolution",
                 hintText: "Choose Resolution",
-                items: ["items"],
+                items: ItemsLists.resolution,
               ),
             ),
           ],
         ),
         const SizedBox(height: 20),
-        Row(
-          children: [
-            Expanded(
-              child: Dropdown(
-                onSaved: (value) {},
-                fieldTitle: "Display Tech",
-                hintText: "Choose Display",
-                items: ["items"],
-              ),
-            ),
-            const SizedBox(width: 20),
-            Expanded(
-              child: Dropdown(
-                onSaved: (value) {},
-                fieldTitle: "Refresh Rate",
-                hintText: "Choose Refresh Rate",
-                items: ["items"],
-              ),
-            ),
-          ],
+        Expanded(
+          child: Dropdown(
+            onSaved: (value) {},
+            fieldTitle: "Refresh Rate",
+            hintText: "Choose Refresh Rate",
+            items: ItemsLists.fps,
+          ),
         ),
         const SizedBox(height: 20),
         Dropdown(
           onSaved: (value) {},
           fieldTitle: "Operating System",
           hintText: "Choose OS",
-          items: ["items"],
+          items: ItemsLists.os,
         ),
         const SizedBox(height: 20),
         Dropdown(
           onSaved: (value) {},
           fieldTitle: "Condition",
           hintText: "Choose Condition",
-          items: ["New", "Like New", "Good"],
+          items: ItemsLists.condition,
         ),
       ],
     );
